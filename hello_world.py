41d369e50f89612d1dd2e9299fc46167b9612a32
davidacostaborges21-stack <davidacostaborges21@gmail.com>
2026-02-03 19:24:14 -0300

    Adicionando Hello World interativo

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+# Pergunta nome e idade
+nome = input("Qual é o seu nome? ")
+idade = input("Quantos anos você tem? ")
+
+# Mensagem engraçada de resposta
+print("Oi " + nome + ", com " + idade + " anos você já tá ficando fera em programação 😎")
